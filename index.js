49ffbee87a499615c6c90912d9e0e3dcdf0af821
Lok <lb.tamang77@gmail.com>
2022-02-04 14:58:17 -0700

    part 1 completed

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-(function() {
-  'use strict';
+const stopButton = document.getElementById('stopButton');
+const slowButton = document.getElementById('slowButton');
+const goButton = document.getElementById('goButton');
 
-  // YOUR CODE HERE
-})();
+const stopLight = document.getElementById('stopLight');
+const slowLight = document.getElementById('slowLight');
+const goLight = document.getElementById('goLight');
+
+
+stopButton.addEventListener('click', () => stopLight.classList.toggle('stop'));
+slowButton.addEventListener('click', () => slowLight.classList.toggle('slow'));
+goButton.addEventListener('click', () => goLight.classList.toggle('go'));
