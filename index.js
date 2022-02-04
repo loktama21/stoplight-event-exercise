f763e3b448100b46404ec8467ba1465357faf5c4
Lok <lb.tamang77@gmail.com>
2022-02-04 15:53:58 -0700

    part 2 completed

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ const goLight = document.getElementById('goLight');
 stopButton.addEventListener('click', () => stopLight.classList.toggle('stop'));
 slowButton.addEventListener('click', () => slowLight.classList.toggle('slow'));
 goButton.addEventListener('click', () => goLight.classList.toggle('go'));
+
+stopButton.addEventListener('mouseenter', () => console.log(`Entered ${stopButton.innerText} button`)); 
+slowButton.addEventListener('mouseenter', () => console.log(`Entered ${slowButton.innerText} button`));
+goButton.addEventListener('mouseenter', () => console.log(`Entered ${goButton.innerText} button`));
+
+stopButton.addEventListener('mouseleave', () => console.log(`Left ${stopButton.innerText} button`)); 
+slowButton.addEventListener('mouseleave', () => console.log(`Left ${slowButton.innerText} button`));
+goButton.addEventListener('mouseleave', () => console.log(`Left ${goButton.innerText} button`));
+
